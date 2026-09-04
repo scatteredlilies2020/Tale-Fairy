26e01460a15fae6a415d68abffabba0122d63c4c
scatteredlilies2020 <314092435+scatteredlilies2020@users.noreply.github.com>
2026-09-04 13:07:58 +0800

    Make Tale Fairy movement self-propelling

--- extension/state.js
+++ extension/state.js
@@ -1,9 +1,9 @@
-import { defaultAuthorBoard, normalizeAuthorBoard, refreshAuthorBoardFromLegacy } from './author-board.js?v=0.12.4';
+import { defaultAuthorBoard, normalizeAuthorBoard, refreshAuthorBoardFromLegacy } from './author-board.js?v=0.12.5';
 import { defaultConductorState, formatConductorContract, normalizeConductorState } from './conductor.js';
 import { defaultPacingState, normalizePacingState } from './pacing.js';
 import { defaultPlannerSchedule, markPlannerCompleted, normalizePlannerSchedule } from './planner-scheduler.js';
-import { defaultBeatDirective, defaultSceneProfile, formatBeatContract, hasUsableBeatDirective, normalizeBeatDirective, normalizeSceneProfile } from './beat-director.js?v=0.12.4';
-import { normalizeDirectorSample } from './director-sampling.js?v=0.12.4';
+import { defaultBeatDirective, defaultSceneProfile, formatBeatContract, hasUsableBeatDirective, normalizeBeatDirective, normalizeSceneProfile } from './beat-director.js?v=0.12.5';
+import { normalizeDirectorSample } from './director-sampling.js?v=0.12.5';
 
 export const STATE_KEY = 'livingWorldGuide';
 export const STATE_VERSION = 54;
